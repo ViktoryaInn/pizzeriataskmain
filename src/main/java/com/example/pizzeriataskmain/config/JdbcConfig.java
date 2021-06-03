ba73c0b282b7d28c0e70ed02625690f5ec0c426d
Viktoria <vicmurrmiu@gmail.com>
2021-06-03 08:21:51 +0500

    some changes

--- src/main/java/com/example/pizzeriataskmain/config/JdbcConfig.java
+++ src/main/java/com/example/pizzeriataskmain/config/JdbcConfig.java
@@ -1,5 +1,6 @@
 package com.example.pizzeriataskmain.config;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
 import org.springframework.data.relational.core.dialect.Dialect;
@@ -7,12 +8,13 @@ import org.springframework.data.relational.core.dialect.PostgresDialect;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 
 @Configuration
-public class SpringDataJdbcConfiguration extends AbstractJdbcConfiguration {
+public class JdbcConfig extends AbstractJdbcConfiguration {
 
     // эта конфигурация нужна, чтобы приложение стартовало, даже если база данных не запущена
 
+    @NotNull
     @Override
-    public Dialect jdbcDialect(NamedParameterJdbcOperations operations) {
+    public Dialect jdbcDialect(@NotNull NamedParameterJdbcOperations operations) {
         return PostgresDialect.INSTANCE;
     }
 }
